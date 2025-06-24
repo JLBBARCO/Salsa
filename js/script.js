5ae4326578ef6e11368da0cfc1c834c00dfe6317
Jose Luiz Bruiani Barco <jose.barco@escola.pr.gov.br>
2025-06-24 12:41:45 -0300

    update varias coisas

--- js/script.js
+++ js/script.js
@@ -143,7 +143,7 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
     <menu>
       <button onclick="menuToggle()" class="menu-button">
-        <span class="material-symbols-outlined burger"> menu </span>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
       </button>
       <div class="menu">
         ${menuLinks}
